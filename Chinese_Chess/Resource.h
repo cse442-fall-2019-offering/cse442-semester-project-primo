52f04116cc27d4c048d01df9031c4aa12f86c26e
yuyangba <yuyangba@buffalo.edu>
2020-03-02 18:15:31 -0500

    updated project

--- Chinese_Chess/Resource.h
+++ Chinese_Chess/Resource.h
@@ -25,7 +25,8 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_
+OURCE_VALUE        138
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
