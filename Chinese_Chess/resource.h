62a236f1c6da2c9d33f2bc441b047e7379e261b5
yinxia Chen <yinxiach@buffalo.edu>
2020-03-09 17:46:55 -0400

    Creating setting dialog

---
+++ Chinese_Chess/resource.h
@@ -0,0 +1,56 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 ChineseChess.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CHINESE_CHESS_DIALOG        102
+#define IDB_BRESTART                    103
+#define IDB_BRETURN                     104
+#define IDB_PCAN2                       105
+#define IDB_PCHAR2                      106
+#define IDB_PBISHOP2                    107
+#define IDB_PKING2                      108
+#define IDB_PADVISOR2                   109
+#define IDB_PHORSE2                     110
+#define IDB_PSOLDIER2                   111
+#define IDB_PCAN1                       112
+#define IDB_PCHAR1                      113
+#define IDB_PBISHOP1                    114
+#define IDB_PKING1                      115
+#define IDB_PADVISOR1                   116
+#define IDB_PHORSE1                     117
+#define IDB_PSOLDIER1                   118
+#define IDB_BOARD                       119
+#define IDB_BITMAP1                     120
+#define IDB_BGP2                        121
+#define IDB_BITMAP2                     124
+#define IDD_DIALOG1                     125
+#define IDR_MAINFRAME                   128
+#define IDB_BGP                         129
+#define IDB_BQUIT                       130
+#define IDB_BSTART                      131
+#define IDB_BVOLUMN                     132
+#define IDB_BVOL1                       135
+#define IDI_GAME_ICON                   137
+#define IDC_BSTART                      1001
+#define IDC_BQUIT                       1003
+#define IDC_BVOL                        1005
+#define IDC_VOLBAR                      1006
+#define IDC_COMBO1                      1007
+#define IDC_LIST1                       1008
+#define IDC_BRETURN                     1009
+#define IDC_BRESTART                    1010
+#define IDC_UI_BUTTON1                  1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        127
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           122
+#endif
+#endif
