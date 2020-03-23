e3b539b29c1863b1591f91bb669ae05ff37e44b2
yinxia Chen <yinxiach@buffalo.edu>
2020-03-23 00:29:18 -0400

    Dialog resource update

--- Chinese_Chess/resource.h
+++ Chinese_Chess/resource.h
@@ -2,6 +2,8 @@
 // Microsoft Visual C++ 生成的包含文件。
 // 供 ChineseChess.rc 使用
 //
+#define IDCANCEL                        2
+#define IDD_Settings                    9
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
 #define IDS_ABOUTBOX                    101
@@ -43,14 +45,15 @@
 #define IDC_BRETURN                     1009
 #define IDC_BRESTART                    1010
 #define IDC_UI_BUTTON1                  1011
+#define IDC_BUTTON2                     1016
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        127
+#define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_CONTROL_VALUE         1017
 #define _APS_NEXT_SYMED_VALUE           122
 #endif
 #endif
