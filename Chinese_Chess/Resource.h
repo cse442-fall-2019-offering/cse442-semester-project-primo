0dba2b5288cd6f083eeb5d436ef216a5c50c2063
Xiaoyu <xzhang92@buffalo.edu>
2020-03-28 17:40:56 -0400

    task #41 add BMG

--- Chinese_Chess/Resource.h
+++ Chinese_Chess/Resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by ChineseChess.rc
+// Microsoft Visual C++ 生成的包含文件。
+// 供 ChineseChess.rc 使用
 //
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
@@ -9,21 +9,15 @@
 #define IDR_MAINFRAME                   103
 #define IDI_GAME_ICON                   104
 #define IDR_PIECE                       105
-
-//Button picture 110 - 199
 #define IDB_BRESTART                    110
 #define IDB_BRETURN                     111
 #define IDB_BQUIT                       112
 #define IDB_BSTART                      113
 #define IDB_BVOLUMN                     114
 #define IDB_BVOL1                       115
-
-//Backgroud picture
 #define IDB_BOARD                       200
 #define IDB_BGP                         201
 #define IDB_BGP2                        202
-
-//Button object 1000 - 1099
 #define IDC_BSTART                      1001
 #define IDC_BQUIT                       1002
 #define IDC_BVOL                        1003
@@ -31,9 +25,6 @@
 #define IDC_BRETURN                     1005
 #define IDC_BRESTART                    1006
 #define IDC_UI_BUTTON1                  1007
-
-
-//pieces picture 1100-1113
 #define IDC_STATIC1                     1008
 #define IDC_STATIC2                     1009
 #define IDC_STATIC3                     1010
@@ -52,15 +43,13 @@
 #define IDB_PADVISOR1                   1111
 #define IDB_PHORSE1                     1112
 #define IDB_PSOLDIER1                   1113
-
-
 #define IDB_SELECTED                    1114
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
