572624cf2083fd0c15598b3e6694a104f04dde20
Xiaoyu <xzhang92@buffalo.edu>
2020-04-13 14:54:24 -0400

    fix turnoff button

--- Chinese_Chess/Resource.h
+++ Chinese_Chess/Resource.h
@@ -38,6 +38,7 @@
 #define IDC_BLOADGAME                   1015
 #define IDC_EDIT_TIME                   1016
 #define IDC_EDIT1                       1017
+#define IDC_BUTTON1                     1019
 #define IDB_PCAN2                       1100
 #define IDB_PCHAR2                      1101
 #define IDB_PBISHOP2                    1102
@@ -60,7 +61,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        117
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_CONTROL_VALUE         1020
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
